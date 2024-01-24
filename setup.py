1ad811a6a2d1c890830ad4c67cb82a6b13553427
Xiang Wang <ramwin@qq.com>
2024-01-24 22:46:57 +0800

    release: 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = "state-machine-2",
-    version = "0.1.0",
+    version = "0.2.0",
     author="Xiang Wang",
     author_email="ramwin@qq.com",
     description="a state machine that support multi process",
